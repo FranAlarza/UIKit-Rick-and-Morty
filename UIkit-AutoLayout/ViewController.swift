7921280c5a05126732be41b4e6d939e0168efa6d
Fran Alarza <franalarza@gmail.com>
2022-06-25 23:58:42 +0200

    Initial Commit

---
+++ UIkit-AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkit-AutoLayout
+//
+//  Created by Fran Alarza on 25/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIkit-AutoLayoutUITests/UIkit_AutoLayoutUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UIkit_AutoLayoutUITestsLaunchTests.swift
+//  UIkit-AutoLayoutUITests
+//
+//  Created by Fran Alarza on 25/6/22.
+//
+
+import XCTest
+
+class UIkit_AutoLayoutUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
